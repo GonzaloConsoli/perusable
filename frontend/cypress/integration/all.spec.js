b5959731b62d0df60cf5bbd4f6f521934902212a
Gonzalo Consoli <gconsoli@mpf.gov.ar>
2023-08-16 15:28:56 -0300

    .

---
+++ frontend/cypress/integration/all.spec.js
@@ -0,0 +1,19 @@
+
+describe('Perusable', function () {
+    it('Displays the home page.', function () {
+        cy.visit('/');
+        cy.get('h1').should('contain', 'Perusable');
+    });
+
+    it('Displays a list of results.', function () {
+        cy.intercept('GET', '**/api/v1/catalog/wines/**', { fixture: 'wines.json' }).as('getWines');
+
+        cy.visit('/');
+        cy.get('input#country').type('US');
+        cy.get('input#points').type('92');
+        cy.get('input#query').type('staglin');
+        cy.get('button').contains('Search').click();
+        cy.wait('@getWines');
+        cy.get('div.card-title').should('contain', 'Cabernet Sauvignon');
+    });
+});
